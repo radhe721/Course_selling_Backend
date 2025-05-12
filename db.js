01ff6bdf04f3c037bbff1c790771cbd4a2954f79
radhe721 <radhewebtech@gmail.com>
2025-05-13 01:01:14 +0530

    Updated schema in db.js file

---
+++ db.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
+const userSchema = new mongoose.Schema({
+    _id: ObjectId,
+    username: { type: String, require: true, unique: true },
+    lastname: { type: String },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true }
+});
+const adminSchema = new mongoose.Schema({
+    _id: ObjectId,
+    username: { type: String, require: true, unique: true },
+    lastname: { type: String },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true }
+});
+const courseSchema = new mongoose.Schema({
+    _id : ObjectId,
+    username: { type: String, require: true, unique: true },
+    title: { type: String },
+    price: { type: Number },
+    imageUrl: { type: String },
+    creatorId : ObjectId
+});
+const purchaseSchema = new mongoose.Schema({
+    courseId : ObjectId,
+    userId : ObjectId,
+    _id : ObjectId
+});
+
+const userModel = mongoose.model('User', userSchema);
+const adminModel = mongoose.model('Admin', adminSchema);
+const courseModel = mongoose.model('Admin', courseSchema);
+const purchaseModel = mongoose.model('Purchase', purchaseSchema);
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {userRouter} = require("./routes/user");
 const {courseRouter} = require("./routes/course");
 const {adminRouter} = require("./routes/admin");
 
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/user", userRouter); 
 app.use("/api/v1/admin", adminRouter);
 app.use("/api/v1/course", courseRouter);
 app.listen(3000);
